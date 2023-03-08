a9c8f43b3e67241f3a5b5f33703c8f168088d647
Serhii Kolodych <kolodych@hotmail.com>
2023-03-08 06:03:27 +0200

    Add files via upload

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, text
+
+
+engine = create_engine(
+    DB_CONN,
+    connect_args={
+        "ssl": {
+            "ca": "/etc/ssl/cert.pem"
+        }
+    })
+
+def all_movies_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM movies"))
+    return result.all()
+
+print(all_movies_from_db())
